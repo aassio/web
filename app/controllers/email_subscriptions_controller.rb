82ffadbdc7e94d5cccd36d9870a19aa19bdbef19
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-09-05 00:42:57 +0300

    Ed's design

--- app/controllers/email_subscriptions_controller.rb
+++ app/controllers/email_subscriptions_controller.rb
@@ -2,7 +2,7 @@ class EmailSubscriptionsController < ApplicationController
 
   def create
     @email_subscription = EmailSubscription.new params[:email_subscription].permit(:email, :name)
-    if @email_subscription.save
+    if params[:agree] == '1' && @email_subscription.save
       render 'create'
     else
       render 'new'
