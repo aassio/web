a124584a6d10178cbc71ce0e94e377e28b329a7e
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-09-18 09:39:47 +0300

    basic auth for staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  if Rails.env.staging?
+    http_basic_authenticate_with name: Rails.application.credentials.basic_auth[:login], password: Rails.application.credentials.basic_auth[:password]
+
+  end
+
 end
