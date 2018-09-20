ed0c3a3c145c11bc11a892af0cd66f48814ca3ca
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-09-20 14:54:34 +0300

    menu

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,15 @@ class WelcomeController < ApplicationController
   def index
     @email_subscription = EmailSubscription.new
   end
+
+
+  def home; end
+  def exchange; end
+  def life; end
+  def ecosystem; end
+
+
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root 'welcome#index'
+
+  get 'exchange', to: 'welcome#exchange'
+  get 'home', to: 'welcome#home'
+  get 'ecosystem', to: 'welcome#ecosystem'
+  get 'life', to: 'welcome#life'
+
   resources :email_subscriptions, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
