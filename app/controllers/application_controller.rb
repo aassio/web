5e8cc84a40cf3f936f055b5b5888fc94f1e50952
Michael Kechinov <mkechinov@Michaels-MacBook-Pro.local>
2018-10-09 00:24:43 +0300

    Trying fix csrf error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
 
   if Rails.env.staging?
     http_basic_authenticate_with name: Rails.application.credentials.basic_auth[:login], password: Rails.application.credentials.basic_auth[:password]
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'aass.io', port: 443 }
+  config.action_mailer.default_url_options = { host: 'stage.aass.io', port: 80 }
 
 
 end
